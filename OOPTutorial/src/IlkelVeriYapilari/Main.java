af80a2bafff6a0ca6c9af0bf78eea9383fdf130d
Berkan Turkoglu <berkan.trkgl35@gamil.com>
2022-10-09 19:45:13 +0300

    Print fonksiyoununun yazimi

--- OOPTutorial/src/IlkelVeriYapilari/Main.java
+++ OOPTutorial/src/IlkelVeriYapilari/Main.java
@@ -1,7 +1,7 @@
 package IlkelVeriYapilari;
 
-import javax.swing.plaf.synth.SynthScrollBarUI;
-
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("Merhaba Dunya ! ");
+    }
 }
